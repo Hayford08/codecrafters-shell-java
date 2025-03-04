27cdb94327e7342ae9d2311d28e15ab15ff0559a
Hayford Akomeah <hayford@mit.edu>
2025-03-03 20:26:58 -0500

    with echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,21 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if (input.substring(0, 4).equals("exit")) {
+            String firstFour = input.substring(0, 4);
+            if (firstFour.equals("exit")) {
                 try {
-                    int exitCode = Integer.parseInt(input.substring(5));
-                    System.exit(exitCode);
+                    System.exit(Integer.parseInt(firstFour.substring(5)));
                 }
                 catch (Exception e) {
                     System.exit(1);
                 }
             }
-            System.out.print(input + ": command not found\n");
+            else if (firstFour.equals("echo")) {
+                System.out.println(input.substring(5));
+            }
+            else {
+                System.out.print(input + ": command not found\n");
+            }
         }
     }
 }
