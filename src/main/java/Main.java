b19e7fc5d5d956e0ebdf54aeb2d7812d87a0ef98
Hayford Akomeah <hayford@mit.edu>
2025-03-03 20:00:50 -0500

    Outputing invalid command always

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,11 @@ public class Main {
         // Uncomment this block to pass the first stage
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input = scanner.nextLine();
+
+            System.out.println(input + ": command not found");
+        }
+        
     }
 }
