b0447e541577585793ea4e92a792ed261330eb7e
Hayford Akomeah <hayford@mit.edu>
2025-03-03 20:31:28 -0500

    With echo fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,22 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            String firstFour = input.substring(0, 4);
-            if (firstFour.equals("exit")) {
+            
+            String[] cmds = input.split(" ", 2);
+            if (cmds.length == 0) {
+                System.out.print(input + ": command not found\n");
+                continue;
+            }
+            if (cmds[0].equals("exit")) {
                 try {
-                    System.exit(Integer.parseInt(firstFour.substring(5)));
+                    System.exit(Integer.parseInt(cmds[1]));
                 }
                 catch (Exception e) {
                     System.exit(1);
                 }
             }
-            else if (firstFour.equals("echo")) {
-                System.out.println(input.substring(5));
+            else if (cmds[0].equals("echo")) {
+                System.out.println(cmds[1]);
             }
             else {
                 System.out.print(input + ": command not found\n");
