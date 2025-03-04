e12d05620df8d4f0ae97411b3806dc49275d64db
Hayford Akomeah <hayford@mit.edu>
2025-03-03 20:24:03 -0500

    adding exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,15 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            if (input.substring(0, 4).equals("exit")) {
+                try {
+                    int exitCode = Integer.parseInt(input.substring(5));
+                    System.exit(exitCode);
+                }
+                catch (Exception e) {
+                    System.exit(1);
+                }
+            }
             System.out.print(input + ": command not found\n");
         }
     }
